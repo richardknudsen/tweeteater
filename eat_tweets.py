ff0d43640282f148ac3f985cbad5338ca9eda90f
Richard Knudsen <richardknudsen@Richards-MacBook-Pro-2.local>
2019-04-25 16:46:31 +0200

    added engagement extraction

--- eat_tweets.py
+++ eat_tweets.py
@@ -2,7 +2,6 @@ import json
 import itertools
 import functools
 from tqdm import tqdm
-##from more_itertools import ilen
 from utils import *
 
 
@@ -112,11 +111,65 @@ def eat_tweetattributes(filepaths, ids, attributes, n_filepaths=None):
     return tweetattributes
 
 
+def get_engagements(tupl, ids):
+    '''
+    workhorse of engagement extraction. extracts engagements from a single tuple (tweet, tweettypes).
+
+    :param tuple tupl: single tuple (tweet, tweettypes) coming out of get_tweettypes
+    :param list ids: list the tweet ids with respect to which engagements are extracted (usually 'original' tweets)
+    :returns list of tuples: engagement information extract from the single tweet, that is
+                            [(tweet ID, tweet ID of reference tweet, engagement type, value), ...], e.g.
+                            [(1, 2, 'favourite_count', 1), ('1', '2', 'retweet_count', 4)], meaning tweet
+                            1 contains the information that the favourite count of tweet 2 is 2 and the
+                            retweet count is 4 at the time tweet 1 was made. engagement types can be
+                            'favourite_count', 'retweet_count', 'reply', or 'quote'. The counts represent
+                            counts as contained in tweet objects, 'reply' and 'quote' are simple in-sample
+                            counts and hence, on the level of a single tweet, always equal to one.
+    '''
+    tweet = tupl[0]
+    tweettypes = tupl[1]
+    tweet_id = tweet.get('id')
+    engagements = []
+    if 'retweet' in tweettypes:
+        retweeted_id = safe_get(tweet, *('retweeted_status', 'id'))
+        if retweeted_id in ids:
+            retweeted_favourite_count = safe_get(tweet, *('retweeted_status', 'favorite_count'))
+            retweeted_retweet_count = safe_get(tweet, *('retweeted_status', 'retweet_count'))
+            engagements.append((tweet_id, retweeted_id, 'favourite_count', retweeted_favourite_count))
+            engagements.append((tweet_id, retweeted_id, 'retweet_count', retweeted_retweet_count))
+    if 'quote' in tweettypes:
+        quoted_id = tweet.get('quoted_status_id')
+        if quoted_id in ids:
+            quoted_favourite_count = safe_get(tweet, *('quoted_status', 'favorite_count'))
+            quoted_retweet_count = safe_get(tweet, *('quoted_status', 'retweet_count'))
+            engagements.append((tweet_id, quoted_id, 'favourite_count', quoted_favourite_count))
+            engagements.append((tweet_id, quoted_id, 'retweet_count', quoted_retweet_count))
+            engagements.append((tweet_id, quoted_id, 'quote', 1))
+    if 'reply' in tweettypes:
+        replied_to_id = tweet.get('in_reply_to_status_id')
+        if replied_to_id in ids:
+            engagements.append((tweet_id, replied_to_id, 'reply', 1))
+    return engagements
 
 
-## engagement scripting (here you need better tests)
-# load tweets
-#tweets = load_tweets_from_filepaths(filepaths,
-#                                    progressbar=True if n_filepaths else False,
-#                                    n_filepaths=n_filepaths)
-#tweettypes = map(get_tweettypes, tweets)
+
+def eat_engagements(filepaths, ids, n_filepaths=None):
+    '''
+    extracts engagements from tweets in files
+
+    :param iterable filepaths: an iterable with the paths of the tweetfiles
+    :param list ids: list the tweet ids with respect to which engagements are extracted (usually 'original' tweets)
+    :param int n_filepaths: optional, if not None, this is used to print progress
+    :returns itertool.chain object: generator-type object of
+                                    [(tweet ID, tweet ID of reference tweet, engagement type, value), ...]. for more
+                                    see documentation of get_engagements
+    '''
+    if not isinstance(ids, set): ids = set(ids)
+    tweets = load_tweets_from_filepaths(filepaths,
+                                        progressbar=True if n_filepaths else False,
+                                        n_filepaths=n_filepaths)
+    tweettypes = map(get_tweettypes, tweets)
+    engagements = map(functools.partial(get_engagements, ids=ids), tweettypes)
+    engagements = filter(lambda engs: len(engs) > 0, engagements)
+    engagements = itertools.chain.from_iterable(engagements)
+    return engagements
--- test_eat_tweets.py
+++ test_eat_tweets.py
@@ -10,7 +10,7 @@ def test_eat_tweettypes():
     n_filepaths = len(list(get_filepaths(directory, extension)))
     subset_func = lambda tweet: True
     tweets = dict(eat_tweettypes(filepaths, subset_func=subset_func, n_filepaths=n_filepaths))
-    assert tweets == load_pickle('test_data/tweets.pickle')
+    assert tweets == load_pickle('test_data/tweettypes.pickle')
 
 
 def test_get_attributes():
@@ -24,7 +24,29 @@ def test_eat_tweetattributes():
     extension = '.jsonl'
     filepaths = get_filepaths(directory, extension)
     n_filepaths = len(list(get_filepaths(directory, extension)))
-    ids = [id_ for id_, types in load_pickle('test_data/tweets.pickle').items()]
+    ids = [id_ for id_, types in load_pickle('test_data/tweettypes.pickle').items()]
     attributes = ['created_at', 'user.screen_name']
     tweet_attributes = eat_tweetattributes(filepaths, ids, attributes, n_filepaths=n_filepaths)
     assert dict(tweet_attributes) == load_pickle('test_data/tweets_attributes.pickle')
+
+
+def test_get_engagements():
+    ids = [2, 3]
+    tweet1 = {'id': 1,
+              'retweeted_status': {'id': 2, 'favorite_count': 3, 'retweet_count': 4},
+              'in_reply_to_status_id': None}
+
+    tweet2 = {'id': 1,
+              'retweeted_status': {'id': 0, 'favorite_count': 3, 'retweet_count': 4},
+              'in_reply_to_status_id': None}
+    tweet3 = {'id': 4,
+              'quoted_status_id': 2,
+              'quoted_status': {'id': 2, 'favorite_count': 7, 'retweet_count': 8},
+              'in_reply_to_status_id': 3}
+    assert get_engagements(get_tweettypes(tweet1), ids=ids) == [(1, 2, 'favourite_count', 3),
+                                                                (1, 2, 'retweet_count', 4)]
+    assert get_engagements(get_tweettypes(tweet2), ids=ids) == []
+    assert get_engagements(get_tweettypes(tweet3), ids=ids) == [(4, 2, 'favourite_count', 7),
+                                                                (4, 2, 'retweet_count', 8),
+                                                                (4, 2, 'quote', 1),
+                                                                (4, 3, 'reply', 1)]
