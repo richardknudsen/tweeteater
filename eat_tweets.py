3a53046d08fd3f2f7d19ab3c16d01a792a73fda6
Richard Knudsen <richardknudsen@Richards-MacBook-Pro-2.local>
2019-04-25 14:30:27 +0200

    added function documentation

--- eat_tweets.py
+++ eat_tweets.py
@@ -7,12 +7,27 @@ from utils import *
 
 
 def load_tweets_from_filepath(filepath):
+    '''
+    loads a single JSON tweet file where each line corresponds to a tweet
+
+    :param str filepath: the filepath of the tweetfile
+    :returns generator: generator containing the tweets as dicts
+    '''
     with open(filepath, 'r') as file:
         tweets = (json.loads(line) for line in file.readlines())
     return tweets
 
 
 def load_tweets_from_filepaths(filepaths, progressbar=False, n_filepaths=None):
+    '''
+    loads tweets from multiple file
+
+    :param iterable filepaths: an iterable with the paths of the tweetfiles
+    :param bool progressbar: whether to print progress, when True, this requires n_filepaths
+    :param int n_filepaths: optional, if not None, this is used to print progress
+    :returns itertool.chain object: generator-type containing the tweets as dicts
+    '''
+    assert not(progressbar == True and n_filepaths == None)
     if progressbar:
         return itertools.chain.from_iterable(map(load_tweets_from_filepath, tqdm(filepaths, total=n_filepaths)))
     else:
@@ -20,6 +35,12 @@ def load_tweets_from_filepaths(filepaths, progressbar=False, n_filepaths=None):
 
 
 def get_tweettypes(tweet):
+    '''
+    extract tweettypes of a tweet and returns tuple of (tweet id, set of types)
+
+    :param dict-like tweet: tweet
+    :returns tuple: (tweet id, set of types), e.g. (1, {'retweet'})
+    '''
     tweettypes = set()
     if 'retweeted_status' in tweet:
         tweettypes.add('retweet')
@@ -34,6 +55,16 @@ def get_tweettypes(tweet):
 
 def eat_tweettypes(filepaths, keep_tweettypes=['original', 'retweet', 'reply', 'quote'],
                    subset_func=lambda tweet: True, n_filepaths=None):
+    '''
+    reads tweets from JSONs, subsets by tweettypes as well as by custom function
+    and returns tweet ID and types
+
+    :param iterable filepaths: an iterable with the paths of the tweetfiles
+    :param list keep_tweettypes: a list (or array-like) of the tweet types to filter, default is all
+    :param func subset_func: function to provide custom subsetting, default is all
+    :param int n_filepaths: optional, if not None, this is used to print progress
+    :returns map: map object with tuples of tweet ID and set of types, e.g. [(1, {'retweet'}), (2, {'original'})]
+    '''
     if not isinstance(keep_tweettypes, set): keep_tweettypes = set(keep_tweettypes)
     assert keep_tweettypes.issubset(set(['original', 'retweet', 'reply', 'quote']))
     tweets = load_tweets_from_filepaths(filepaths,
@@ -46,6 +77,13 @@ def eat_tweettypes(filepaths, keep_tweettypes=['original', 'retweet', 'reply', '
 
 
 def get_attributes(tweet, attributes):
+    '''
+    extracts attributes from a tweet object
+
+    :param dict tweet: the tweet object
+    :param list attributes: list of attributes to extract, nested attributes separated with '.', e.g. 'user.screen_name'
+    :returns tuple: returns tuple of (tweet ID, [attribute[0], attribute[1], ...])
+    '''
     values = []
     for attribute in attributes:
         keys = attribute.split('.')
@@ -54,6 +92,15 @@ def get_attributes(tweet, attributes):
 
 
 def eat_tweetattributes(filepaths, ids, attributes, n_filepaths=None):
+    '''
+    extracts tweet attributes for a set of tweets specified by ID from tweets stored in filepaths
+
+    :param iterable filepaths: an iterable with the paths of the tweetfiles
+    :param iterable ids: list (or set) with target tweets
+    :param list attributes: list of attributes to extract, nested attributes separated with '.', e.g. 'user.screen_name'
+    :param int n_filepaths: optional, if not None, this is used to print progress
+    :returns map: containing tuples of (tweet ID, [attribute[0], attribute[1], ...])
+    '''
     if not isinstance(ids, set): ids = set(ids)
     tweets = load_tweets_from_filepaths(filepaths,
                                         progressbar=True if n_filepaths else False,
@@ -61,19 +108,3 @@ def eat_tweetattributes(filepaths, ids, attributes, n_filepaths=None):
     tweets = filter(lambda tweet: tweet.get('id') in ids, tweets)
     tweets = map(functools.partial(get_attributes, attributes=attributes), tweets)
     return tweets
-
-
-#weets = eat_tweettypes(filepaths, extension=extension, n_filepaths=n_filepaths)
-
-
-
-
-
-# test script eat tweettypes args
-#irectory = '/Volumes/Data/mt_data/ger/2017-09-24'
-#xtension = '.jsonl'
-#ilepaths = get_filepaths(directory, extension)
-#_filepaths = len(list(get_filepaths(directory, extension)))
-#ubset_func = lambda tweet: True
-#weets = eat_tweettypes(filepaths, extension=extension, n_filepaths=n_filepaths)
-#
